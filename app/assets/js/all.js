f878d1018da7836ca598c68fd35ff24559433264
youting <youting.gmail.com>
2021-12-13 13:58:59 +0800

    completed pc index

---
+++ app/assets/js/all.js
@@ -0,0 +1,20 @@
+$(function() {
+  const swiperCourse = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay: {
+      loop: true,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 1.4,
+        spaceBetween: 30
+      }
+    },
+  });
+});
